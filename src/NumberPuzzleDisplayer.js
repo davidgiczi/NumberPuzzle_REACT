dfe0055159c0896e3fb7597cfecedf0c1b2dbdac
david.giczi@gmail.com <david.giczi@gmail.com>
2022-04-27 13:25:48 +0200

    Complete

--- src/NumberPuzzleDisplayer.js
+++ src/NumberPuzzleDisplayer.js
@@ -1,28 +1,39 @@
-import { numberStore, shuffleNumberSquare} from "./NumberPuzzleLogic";
+import { shuffleNumberSquare, step, isTheEndOfTheGame } from './NumberPuzzleLogic';
+import { numberStore, sumShuffleValue, initSumShuffleValue } from './NumberPuzzleLogic';
+import welldone from './icon/welldone.jpg';
 import { useState } from 'react'
 let roundValueByUser;
+
 function GuessNumberField(props){
 return(<>
-    <button className="Number-field" style={{visibility: props.visible}}>{props.number}</button>
+    <button className='Number-field' onClick={props.onClick} 
+    style={{visibility: props.visible, color: props.color, backgroundImage : props.image}}>{props.number}</button>
     </>)
 }
 
 function GuessNumberBoard(){
     const [boardNumbers, setBoardNumbers] = useState(numberStore);
+    const [btnText, setBtnText] = useState('Kever');
     let [roundValue, setRoundValue] = useState(20);
-    const [btnText, setBtnText] = useState('Start');
-
+    let [inputValue, setInputValue] = useState(20);
+    let [clickValue, setClickValue] = useState(0);
+    const [backgroundColor, setBackgroundColor] = useState('#008000');
+    const [backgroundImage, setBackgroundImage] = useState('');
+    const [hidden, setHidden] = useState('hidden');
+    
     const startGame = () => {
-    let inputValue = document.getElementById('rounds').value;
-    if(parseInt(inputValue) === 0){
+    setBackgroundColor('#008000');
+    setHidden('hidden');
+    setClickValue(0);
+    if(inputValue === 0){
         setRoundValue(roundValueByUser);
-        setBtnText('Start');
+        setInputValue(roundValueByUser);
+        setBtnText('Kever');
         return;
     }
     else {
         roundValueByUser = inputValue;
     }
-
     const intro = setInterval(() =>{
         if( roundValue === 0 ){
             setBtnText('Újra');
@@ -32,29 +43,55 @@ function GuessNumberBoard(){
         shuffleNumberSquare();
         setBoardNumbers(() => [...numberStore]);
         setRoundValue(--roundValue);
+        setInputValue(--inputValue);
         }, 500);
     }
     
     const setShuffleValue = (event) => {
         if( 0 < event.target.value ){
         setRoundValue(event.target.value);
+        setInputValue(event.target.value);
          }
-        else {
-        setRoundValue(20);
-         }
+    }
+
+    const stepPlayer = (clickedNumberValue) => {
+       setClickValue(++clickValue);
+       step(clickedNumberValue);
+       setBoardNumbers(() => [...numberStore]);
+       evaluateResult();
+    }
+
+    const evaluateResult = () => {
+
+        if(isTheEndOfTheGame()){
+            setHidden('');
+            setBackgroundColor('#a52a2a');
+            setBackgroundImage('url(' + welldone + ')');
+       const evaluate = sumShuffleValue >= clickValue ? "Gratulálunk!" : "Sebaj, legközelebb!";
+       setTimeout(() => {alert(evaluate +
+            "\n\nMaximálisan szükséges lépések száma: " + sumShuffleValue + " db" +
+            "\nLépéseid száma: " + clickValue + " db"); initSumShuffleValue()}, 1000);
+        }
     }
 
     return(<>
         <div className='Game-board'>
-       {boardNumbers.map((value) => <GuessNumberField  key={value} number={value} visible={value === 0 ? 'hidden' : ''}/>)}
+       {boardNumbers.map((value) =>
+        <GuessNumberField onClick={() => stepPlayer(value)} key={value} 
+        color={value !== 0 ? backgroundColor : 'transparent'} 
+        image={value === 0 ? backgroundImage : ''} number={value} visible={value === 0 ? hidden : ''}/>)}
        <Button onClick={startGame} text={btnText}/>
-       <input className="Round-field" id="rounds" value={roundValue} onChange={setShuffleValue}></input>
+       <InputField onChange={setShuffleValue} roundValue={roundValue}/>
         </div> 
         </>)
 }
 
 function Button(props){
-    return (<button onClick={props.onClick} >{props.text}</button>)
+    return (<button className='Btn-class' onClick={props.onClick} >{props.text}</button>)
+}
+
+function InputField(props){
+    return (<input className='Round-field' value={props.roundValue} onChange={props.onChange}></input>)
 }
 
 export default GuessNumberBoard;
--- src/NumberPuzzleLogic.js
+++ src/NumberPuzzleLogic.js
@@ -1,54 +1,136 @@
 let numberStore = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
+const COL_OF_DISP = 4;
+let sumShuffleValue = 0;
 
 const numberSquareIndexes = [
-                            [0, 1, 4, 5],
-                            [1, 2, 5, 6],
-                            [2, 3, 6, 7],
-                            [4, 5, 8, 9],
-                            [5, 6, 10, 11],
-                            [6, 7, 11, 12],
-                            [8, 9, 12, 13],
-                            [9, 10, 13, 14],
-                            [10, 11, 14, 15]
+                            [0, 1, 5, 4],
+                            [1, 2, 6, 5],
+                            [2, 3, 7, 6],
+                            [4, 5, 9, 8],
+                            [5, 6, 10, 9],
+                            [6, 7, 11, 10],
+                            [8, 9, 13, 12],
+                            [9, 10, 14, 13],
+                            [10, 11, 15, 14]
                                         ];
 
-function shuffleNumberSquare(){
+function getNumberSquareIndexArrayValue(){
+    let numberSquareIndexArrayValue = 0;
+    let firstIndex = numberSquareIndexes[numberSquareIndexArrayValue][0];
+    let secondIndex = numberSquareIndexes[numberSquareIndexArrayValue][1];
+    let thirdIndex = numberSquareIndexes[numberSquareIndexArrayValue][2];
+    let forthIndex = numberSquareIndexes[numberSquareIndexArrayValue][3];
+    while( numberStore[firstIndex] !== 0 && numberStore[secondIndex] !== 0 && 
+        numberStore[thirdIndex] !== 0 && numberStore[forthIndex] !== 0 ){
+        numberSquareIndexArrayValue = Math.floor(Math.random() * 9);
+        firstIndex = numberSquareIndexes[numberSquareIndexArrayValue][0];
+        secondIndex = numberSquareIndexes[numberSquareIndexArrayValue][1];
+        thirdIndex = numberSquareIndexes[numberSquareIndexArrayValue][2];
+        forthIndex = numberSquareIndexes[numberSquareIndexArrayValue][3];
+    } 
+    return numberSquareIndexArrayValue;
+}
 
-    let numberSquareIndex = Math.floor(Math.random() * 9);
+function shuffleNumberSquare(){
+    const numberSquareIndexArrayValue = getNumberSquareIndexArrayValue();
+    const numberSquareArray = numberSquareIndexes[numberSquareIndexArrayValue];
+    const firstIndex = numberSquareArray[0];
+    const secondIndex = numberSquareArray[1];
+    const thirdIndex = numberSquareArray[2];
+    const forthIndex = numberSquareArray[3];
     let numberSquare = [];
-    numberSquare.push(numberStore[numberSquareIndexes[numberSquareIndex][0]]);
-    numberSquare.push(numberStore[numberSquareIndexes[numberSquareIndex][1]]);
-    numberSquare.push(numberStore[numberSquareIndexes[numberSquareIndex][2]]);
-    numberSquare.push(numberStore[numberSquareIndexes[numberSquareIndex][3]]);
+    numberSquare.push(numberStore[firstIndex]);
+    numberSquare.push(numberStore[secondIndex]);
+    numberSquare.push(numberStore[thirdIndex]);
+    numberSquare.push(numberStore[forthIndex]);
     let shuffleValue = Math.floor(Math.random() * 4);
+    sumNeedFullClick(shuffleValue);
     if( shuffleValue === 1 ){
         let temp = numberSquare[0];
-        numberSquare[0] = numberSquare[2];
-        numberSquare[2] = numberSquare[3];
-        numberSquare[3] = numberSquare[1];
+        numberSquare[0] = numberSquare[3];
+        numberSquare[3] = numberSquare[2];
+        numberSquare[2] = numberSquare[1];
         numberSquare[1] = temp;
     }
     else if( shuffleValue === 2 ){
         let temp = numberSquare[0];
-        numberSquare[0] = numberSquare[3];
-        numberSquare[3] = temp;
-        temp = numberSquare[1];
-        numberSquare[1] = numberSquare[2];
+        numberSquare[0] = numberSquare[2];
         numberSquare[2] = temp;
+        temp = numberSquare[1];
+        numberSquare[1] = numberSquare[3];
+        numberSquare[3] = temp;
     }
     else if ( shuffleValue === 3 ){
         let temp = numberSquare[0];
         numberSquare[0] = numberSquare[1];
-        numberSquare[1] = numberSquare[3];
-        numberSquare[3] = numberSquare[2];
-        numberSquare[2] = temp;
+        numberSquare[1] = numberSquare[2];
+        numberSquare[2] = numberSquare[3];
+        numberSquare[3] = temp;
     }
 
-    numberStore[numberSquareIndexes[numberSquareIndex][0]] = numberSquare[0];
-    numberStore[numberSquareIndexes[numberSquareIndex][1]] = numberSquare[1];
-    numberStore[numberSquareIndexes[numberSquareIndex][2]] = numberSquare[2];
-    numberStore[numberSquareIndexes[numberSquareIndex][3]] = numberSquare[3];
+    numberStore[numberSquareIndexes[numberSquareIndexArrayValue][0]] = numberSquare[0];
+    numberStore[numberSquareIndexes[numberSquareIndexArrayValue][1]] = numberSquare[1];
+    numberStore[numberSquareIndexes[numberSquareIndexArrayValue][2]] = numberSquare[2];
+    numberStore[numberSquareIndexes[numberSquareIndexArrayValue][3]] = numberSquare[3];
 }
 
-    export {numberStore};
-    export {shuffleNumberSquare};                                    
+function sumNeedFullClick(rotateValue){ 
+if( rotateValue === 1 || rotateValue === 3 ){
+    sumShuffleValue += 3;
+}
+else if( rotateValue === 2 ){
+    sumShuffleValue += 6;
+}
+}
+
+function step(clickedValue){
+if( !isClickAble(clickedValue) ) {
+    alert("Nem léptethető mező!")
+    return;
+}
+else {
+    changeNumberValue(clickedValue);
+    }
+}
+
+function isClickAble(clickedValue){
+    const clickedValueIndex = numberStore.indexOf(clickedValue);
+    let row = clickedValueIndex % COL_OF_DISP;
+    let col = Math.floor(clickedValueIndex / COL_OF_DISP);  
+   if(row - 1 >= 0 && numberStore[col * COL_OF_DISP +  row - 1] === 0){
+    return true;
+   }
+   else if(row + 1 < COL_OF_DISP && numberStore[col * COL_OF_DISP +  row + 1] === 0){
+    return true;
+   }
+   else if(col - 1 >= 0 && numberStore[(col - 1) * COL_OF_DISP +  row] === 0){
+    return true;
+   }
+   else if(col + 1 < COL_OF_DISP && numberStore[(col + 1) * COL_OF_DISP + row] === 0){
+    return true;
+   }
+    return false;
+}
+
+function changeNumberValue(clickedValue){
+const clickedValueIndex = numberStore.indexOf(clickedValue);
+const zeroValueIndex = numberStore.indexOf(0);
+numberStore[zeroValueIndex] = clickedValue;
+numberStore[clickedValueIndex] = 0;
+}
+
+function isTheEndOfTheGame(){
+for(let i = 0; i < numberStore.length - 2; i++){
+    if(numberStore[i] + 1 !== numberStore[ i + 1 ]){
+        return false;
+    }
+}
+return true;
+}
+
+function initSumShuffleValue(){
+    sumShuffleValue = 0;
+}
+
+    export {numberStore, sumShuffleValue};
+    export {shuffleNumberSquare, step, isTheEndOfTheGame, initSumShuffleValue};                                    
