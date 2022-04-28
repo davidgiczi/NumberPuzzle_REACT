91d9d5bed9048ccf9de5e19909e7243db7c7404b
david.giczi@gmail.com <david.giczi@gmail.com>
2022-04-28 12:40:58 +0200

    It is complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import GuessNumberBoard  from './NumberPuzzleDisplayer';
-
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     <GuessNumberBoard/>
+      { <BrowserRouter>
+<Routes>
+  <Route path='/numberpuzzle' element = {<GuessNumberBoard/>}/>
+</Routes>
+</BrowserRouter>}
     </div>
   );
 }
